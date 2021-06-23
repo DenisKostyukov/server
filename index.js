4a66c83c55b51b3ca8f4efcd76a19f3fd98cf9e0
DenisKostyukov <kostukov08@gmail.com>
2021-06-23 12:14:33 +0300

    add user example

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
-
+app.use(express.json())
 const PORT = 3000
 
-app.listen(PORT,()=>{
+app.get('/', (req,res)=>{
+  res.send("Hello world")
+})
+
+app.get('*', (req,res)=>{
+  res.send('ERROR 404')
+})
+
+app.post('/',(req,res)=>{
+  console.log(req.body)
+})
+
+app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} port`)
 })
